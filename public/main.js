ae41c1d038ed0ed0990d1f0261f65206cd17db9b
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2023-09-19 10:49:27 +0300

    m

--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
 let header = document.querySelector("header");
 let triggered = false;
 document.addEventListener("scroll", () => {
-  console.log(header.offsetHeight);
   if (!triggered && scrollY > 70) {
-    header.style.cssText = "transition: ease-in-out 0.5s; padding-bottom:0.75rem; padding-top:0.75rem; ";
+    header.style.cssText = "transition: ease-in-out 0.5s; padding-bottom:0.75rem; padding-top:0.75rem;";
     triggered = true;
+  } else if (triggered && scrollY < 70) {
+    header.style.cssText = "transition: ease-in-out 0.5s; padding-bottom:1.5rem; padding-top:1.5rem;";
+    triggered = false;
   }
 });
